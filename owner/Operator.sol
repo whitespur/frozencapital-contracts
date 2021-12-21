6a98b8630010dd85ae5d1a287e35712e810cd2ef
Frozen Capital Dev <0x01447e943253E91877b0D6933946157Bd3145D85@ethmail.cc>
2021-12-22 00:41:17 +0400

    Fixed operator transfer event (error was in original tomb contracts as well)

--- owner/Operator.sol
+++ owner/Operator.sol
@@ -34,7 +34,7 @@ contract Operator is Context, Ownable {
 
     function _transferOperator(address newOperator_) internal {
         require(newOperator_ != address(0), "operator: zero address given for new operator");
-        emit OperatorTransferred(address(0), newOperator_);
+        emit OperatorTransferred(_operator, newOperator_);
         _operator = newOperator_;
     }
 }
